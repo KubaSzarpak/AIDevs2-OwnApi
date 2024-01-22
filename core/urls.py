ff136f69c640995aa18725744a2eac5c69d5a00f
Jakub Szarpak <kuba.szarpak02@gmail.com>
2024-01-22 19:52:20 +0100

    Add google task version 0.1

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("chatbot/", views.Chatbot.as_view(), name="chatbot"),
+    path("google/", views.Google.as_view(), name="google"),
 ]
--- core/views.py
+++ core/views.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.decorators import action
+from serpapi import GoogleSearch
 
 # Create your views here.
 
@@ -34,4 +35,22 @@ class Chatbot(APIView):
             ]
         ).choices[0].message.content
 
+        return HttpResponse(json.dumps({'reply': response}), status=status.HTTP_200_OK, charset='utf-8')
+
+
+class Google(APIView):
+    def post(self, request):
+        question = self.request.data.get('question', '')
+
+        params = {
+            'q': question,
+            'location': 'Poland',
+            'hl': 'pl',
+            'gl': 'pl',
+            'api_key': '53087c7bf1cae0f7a11d62d01d3f2021dd2cd892ffcb47a0b61e1c70a00e4018'
+        }
+
+        google_search = GoogleSearch(params)
+
+        response = google_search.get_dict()['search_metadata']['google_url']
         return HttpResponse(json.dumps({'reply': response}), status=status.HTTP_200_OK, charset='utf-8')
